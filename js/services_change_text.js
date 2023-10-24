6fe6d8491739e837e73eaeb3976267d6270fbd18
RahymCharyyev <r569837@gmail.com>
2023-10-24 12:33:44 +0500

    ✨feature: Added description text to services

--- js/services_change_text.js
+++ js/services_change_text.js
@@ -1,4 +1,21 @@
-function changeText(newText) {
-  let leftTextElement = document.querySelector('.services__left-text');
-  leftTextElement.textContent = newText;
+let accordions = document.querySelectorAll('.services__right-accordion');
+let panels = document.querySelectorAll('.services__right-accordionPanel');
+
+for (let i = 0; i < panels.length; i++) {
+  panels[i].style.maxHeight = null;
+}
+
+accordions[0].classList.add('active');
+panels[0].style.maxHeight = panels[0].scrollHeight + 'px';
+
+for (let i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  });
 }
